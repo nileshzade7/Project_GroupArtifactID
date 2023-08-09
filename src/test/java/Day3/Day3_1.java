ce88c313bf1e3ca4f14061d61cc5b4ed4b00cc15
nileshzade7 <nileshzade7@gmail.com>
2023-08-09 06:39:54 +0000

    Initial Project

---
+++ src/test/java/Day3/Day3_1.java
@@ -0,0 +1,17 @@
+package Day3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day3_1 {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+
+	}
+
+}
